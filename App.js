78c8a86c20f63ad557130a12f338d05d85e65dcd
leannegass <leannegass@gmail.com>
2023-05-16 19:53:55 +0100

    added filtered city

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 // Import data and WeatherCard here
 import cities from './data';
-import WeatherCard from './components/WeatherCard'
+import WeatherCard from './components/WeatherCard';
+import Location from './components/Location';
 
 function App() {
     const [location, setLocation] = useState('Paris')
@@ -9,12 +10,16 @@ function App() {
         <>
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
+
+            <Location  data={cities} location={location} setLocation={setLocation}/>
+            
             <div className = "app">
+            
+                
+            
                 {cities.map(city => <WeatherCard data={city}/>)}
             </div>
-            {/* <div>
-                <Location data={cities} location={location} setLocation={setLocation}/>
-            </div> */}
+            
             
             
         </>
--- components/Location.js
+++ components/Location.js
@@ -1,23 +1,31 @@
 import React, {useState} from 'react';
-import {sunny,Rainy,PartlyCloudy,Cloudy} from "../assets/Sunny.svg";
+import sunny from "../assets/Sunny.svg";
+import rainy from "../assets/Rainy.svg";
+import partlyCloudy from "../assets/PartlyCloudy.svg";
+import cloudy from "../assets/Cloudy.svg";
+import snowy from "../assets/Snowy.svg";
 
-function Location(data,location, setLocation){
-    //const city = data.find(data.city => data.city === location )
-    //console.log(city)
+
+function Location({data,location, setLocation}){
+    let weatherIcon = { "Sunny" : sunny,"Rainy" : rainy, "Partly cloudy" : partlyCloudy, "Cloudy" : cloudy, "Snowy" : snowy};
+    // console.log(data[0].city)
+    // find() returning undefined so filtered array to find matching object
+    const foundCity = data.filter(element => element.city === "Paris" )
+    console.log(foundCity[0].city)
 
     return (
         <> 
            
-           {/* <div className = "card">
-           <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
-           </div>
-           <div class="card-body">
-            <h3 className="card-title">{city.city}</h3>
-            <h5 className="card-text">{city.temperature}</h5>
-            <h5 className="card-text">{city.forecast}</h5>
+           <div className = "card">
+               <div className = "img-container">
+                   <img className="card-img-top" src = {weatherIcon[foundCity[0].forecast]} alt="Card image cap" id = "icon"/>
+                </div>
+                <div class="card-body">
+                    <h3 className="card-title">{foundCity[0].city}</h3>
+                    <h5 className="card-text">{foundCity[0].temperature}</h5>
+                    <h5 className="card-text">{foundCity[0].forecast}</h5>
+                </div>
             </div>
-            </div> */}
             
         </>
 
